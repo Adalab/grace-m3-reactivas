e038edfa5ef76028ebac9959e8ece0bf5c6d5a04
anamontiaga <amontiaga@gmail.com>
2019-09-09 15:02:02 +0200

    revert maindescription to dummy

--- src/components/MainDescription.js
+++ src/components/MainDescription.js
@@ -1,18 +1,16 @@
 import React from "react";
 
-class MainDescription extends React.Component {
-  render() {
-    return (
-      <div>
-        <img className="main_img" src={this.props.foto} alt={this.props.mainTitle} />
-        <div className="main_description">
-          <h1 className="main_title">{this.props.mainTitle}</h1>
-          <p className="main_text">{this.props.mainText}</p>
-        </div>
+const MainDescription = props => {
+  return (
+    <div>
+      <img className="main_img" src={props.foto} alt={props.mainTitle} />
+      <div className="main_description">
+        <h1 className="main_title">{props.mainTitle}</h1>
+        <p className="main_text">{props.mainText}</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 // Ana: he tenido que introducir un div para poder usar el componente. Cuando hagamos le scss, hay que ponerle una clase para centrar el texto.
 
