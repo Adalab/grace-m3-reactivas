8f4878f7fd827f168b58f4500c0a00e1e6ed6cfe
Samantha Cancino <samanthacancino@gmail.com>
2019-09-09 23:58:21 +0200

    start using react router

---
+++ src/components/Landing.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Route, Switch } from 'react-router-dom';
+import logoAdalab from "../images/logo-adalab-80px.png";
+import awesomeProfilePic from "../images/tarjetas-molonas.svg";
+import "../stylesheets/scss/main.scss";
+import MainButton from "./MainButton";
+
+class Landing extends React.Component {
+    render(){
+  return (
+    <main className="main">
+    <div className="main_content">
+        <img className="main_img" src={awesomeProfilePic} />
+        <div className="main_description">
+             <h1 className="main_title">Crea tu tarjeta de visita</h1>
+
+            <p className="main_text">Crea mejores contactos profesionales de forma fácil y cómoda</p>
+        </div>
+        <div className="main_actions">
+            <div className="main_action"> <i className="main_action_icon far fa-object-ungroup"></i>
+
+                <p className="main_action_item">Diseña</p>
+            </div>
+            <div className="main_action"> <i className="main_action_icon far fa-keyboard"></i>
+
+                <p className="main_action_item">Rellena</p>
+            </div>
+            <div className="main_action"> <i className="main_action_icon fas fa-share-alt"></i>
+
+                <p className="main_action_item">Comparte</p>
+            </div>
+        </div>
+        <MainButton />
+    </div>
+<div className="footer">
+<small className="footer_small"> Awesome profile-cards @ Sense.JS 2019</small>
+    <a
+    className="footer_link" href="http://www.adalab.es" target="_blank">
+        <img className="footer_img" src={logoAdalab} alt="logo Adalab"/>
+        </a>
+</div>
+</main>
+
+  );
+  }
+}
+
+export default Landing;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter } from 'react-router-dom';
 import "./stylesheets/index.scss";
-import App from "./components/App";
+import Landing from "./components/Landing";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <HashRouter>
+        <Landing />
+    </HashRouter>,
+     document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
