ce23786453e49b12a163c16493096a8af297a654
laurapareja <lauraparejalmodovar@gmail.com>
2019-09-15 00:37:14 +0200

    CardPreview & SocialLinkCard sass ready

--- src/components/SocialLinkCardPreview.js
+++ src/components/SocialLinkCardPreview.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import '../stylesheets/scss/components/SocialLinkCardPreview.scss';
 
-const SocialLinkCardPreview = (props) => {
+const SocialLinkCardPreview = props => {
   return (
     <li className="card_footer_item">
       {' '}
-      <a className={props.linkClass} href="" data-placeholder="" data-prefix={props.socialLink}target="_blank">
-        <i className={props.iconCard}></i>
+      <a
+        className={props.linkClass}
+        href=""
+        data-placeholder=""
+        data-prefix={props.socialLink}
+        target="_blank"
+      >
+        <i className={props.iconCard} />
       </a>
     </li>
   );
