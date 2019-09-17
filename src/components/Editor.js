f2c13e0676e26a7e407a7c388780953706b7485e
saramarin23 <saramarin.econ@gmail.com>
2019-09-17 20:49:40 +0200

    reset button works

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,79 +1,99 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import logoAdalab from '../images/logo-adalab-80px.png';
-import awesomeProfilePic from '../images/tarjetas-molonas.svg';
-import Header from './Header';
-import InputContainer from './InputContainer.js';
-import ResetButton from './ResetButton';
-import ShareButton from './ShareButton';
-import Collapsibles from './Collapsibles.js';
-import PalettesContainer from './PalettesContainer.js';
-import Footer from './Footer';
-import CardPreview from './CardPreview';
+import React from "react";
+import { Link } from "react-router-dom";
+import logoAdalab from "../images/logo-adalab-80px.png";
+import awesomeProfilePic from "../images/tarjetas-molonas.svg";
+import Header from "./Header";
+import InputContainer from "./InputContainer.js";
+import ResetButton from "./ResetButton";
+import ShareButton from "./ShareButton";
+import Collapsibles from "./Collapsibles.js";
+import PalettesContainer from "./PalettesContainer.js";
+import Footer from "./Footer";
+import CardPreview from "./CardPreview";
 
 class Editor extends React.Component {
   constructor(props) {
     super(props);
     const localStorageData = this.getData();
-    this.state = localStorageData === null ? this.getInitialState() : localStorageData;
+    this.state =
+      localStorageData === null ? this.getInitialState() : localStorageData;
     this.updateCheckboxColor = this.updateCheckboxColor.bind(this);
     this.saveData = this.saveData.bind(this);
+    this.clearForm = this.clearForm.bind(this);
+    this.resetData = this.resetData.bind(this);
   }
 
   getInitialState() {
     return {
-      userFullName: '',
-      userJob: '',
-      userPhone: '',
-      userEmail: '',
-      userLinkedin: '',
-      userGithub: '',
-      palette: '1',
+      userFullName: "",
+      userJob: "",
+      userPhone: "",
+      userEmail: "",
+      userLinkedin: "",
+      userGithub: "",
+      palette: "1"
     };
   }
 
+  clearForm() {
+    this.forceUpdate();
+    this.setState({
+      userFullName: "",
+      userJob: "",
+      userPhone: "",
+      userEmail: "",
+      userLinkedin: "",
+      userGithub: ""
+    });
+    this.resetData();
+  }
+
+  resetData() {
+    localStorage.clear();
+  }
+
   updateEventInfo = event => {
-    let key = event.currentTarget.name;
+    let key = event.currentTarget.id;
     let userInfo = event.target.value;
     this.setState(
       {
-        [key]: `${userInfo}`,
+        [key]: `${userInfo}`
       },
       this.saveData
     );
   };
   updatePreviewEmail() {
-    if (this.state.userEmail === '') {
-      return '';
+    if (this.state.userEmail === "") {
+      return "";
     } else {
       return `mailto:${this.state.userEmail}`;
     }
   }
 
   updatePreviewName() {
-    if (this.state.userFullName === '') {
-      return 'Nombre y Apellidos';
+    if (this.state.userFullName === "") {
+      return "Nombre y Apellidos";
     } else {
       return `${this.state.userFullName}`;
     }
   }
   updatePreviewJob() {
-    if (this.state.userJob === '') {
-      return 'Front end developer';
+    if (this.state.userJob === "") {
+      return "Front end developer";
     } else {
       return `${this.state.userJob}`;
     }
   }
   updatePreviewLinkedin() {
-    if (this.state.userLinkedin === '') {
-      return '';
+    if (this.state.userLinkedin === "") {
+      return "";
     } else {
       return `https://www.linkedin.com/in/${this.state.userLinkedin}`;
     }
   }
   updatePreviewGithub() {
-    if (this.state.userGithub === '') {
-      return '';
+    if (this.state.userGithub === "") {
+      return "";
     } else {
       return `https://www.github.com/${this.state.userGithub}`;
     }
@@ -81,65 +101,114 @@ class Editor extends React.Component {
 
   updateCheckboxColor(event) {
     const paletteSelected = event.target.value;
-    this.setState({palette: `${paletteSelected}`}, this.saveData);
+    this.setState({ palette: `${paletteSelected}` }, this.saveData);
   }
 
   saveData() {
-    localStorage.setItem('info', JSON.stringify(this.state));
+    localStorage.setItem("info", JSON.stringify(this.state));
   }
 
   getData() {
-    return JSON.parse(localStorage.getItem('info'));
+    return JSON.parse(localStorage.getItem("info"));
   }
 
   render() {
     const classColor = `card_content palette${this.state.palette}`;
     return (
       <div>
-        <Header url="/" foto={awesomeProfilePic} destiny="_self" alt="Awesome Profile Cards" />
+        <Header
+          url="/"
+          foto={awesomeProfilePic}
+          destiny="_self"
+          alt="Awesome Profile Cards"
+        />
         <section class="editor_container">
           <main class="editor_main">
             <section class="editor_card">
               <main class={classColor}>
                 <div class="container">
-                  <ResetButton icon="far fa-trash-alt trash_icon" name="Reset" />
-                  <CardPreview fullnameClass="js-name card_name" fullname={this.updatePreviewName()} jobClass="js-job card_job" jobCard={this.updatePreviewJob()} imageCard="card_img js__profile-image" userPhone={this.state.userPhone} userEmail={this.updatePreviewEmail()} linkedinLink={this.updatePreviewLinkedin()} githubLink={this.updatePreviewGithub()} />
+                  <ResetButton
+                    icon="far fa-trash-alt trash_icon"
+                    name="Reset"
+                    action={this.clearForm}
+                  />
+                  <CardPreview
+                    fullnameClass="js-name card_name"
+                    fullname={this.updatePreviewName()}
+                    jobClass="js-job card_job"
+                    jobCard={this.updatePreviewJob()}
+                    imageCard="card_img js__profile-image"
+                    userPhone={this.state.userPhone}
+                    userEmail={this.updatePreviewEmail()}
+                    linkedinLink={this.updatePreviewLinkedin()}
+                    githubLink={this.updatePreviewGithub()}
+                  />
                 </div>
               </main>
             </section>
             <section class="editor_form js-form">
               <section class="design_section js-collapsible">
-                <Collapsibles icon="legend_icon far fa-object-ungroup" title="Diseña" arrow="fas fas fa-chevron-up legend_arrow">
-                  <PalettesContainer updateCheckboxColor={this.updateCheckboxColor}></PalettesContainer>
+                <Collapsibles
+                  icon="legend_icon far fa-object-ungroup"
+                  title="Diseña"
+                  arrow="fas fas fa-chevron-up legend_arrow"
+                >
+                  <PalettesContainer
+                    updateCheckboxColor={this.updateCheckboxColor}
+                  ></PalettesContainer>
                 </Collapsibles>
               </section>
               <section class="fill-in_section js-collapsible">
-                <Collapsibles icon="far fa-keyboard legend_icon" title="Rellena" arrow="fas fas fa-chevron-up legend_arrow">
-                  <InputContainer updateEventInfo={this.updateEventInfo} data={this.state} />
+                <Collapsibles
+                  icon="far fa-keyboard legend_icon"
+                  title="Rellena"
+                  arrow="fas fas fa-chevron-up legend_arrow"
+                >
+                  <InputContainer
+                    updateEventInfo={this.updateEventInfo}
+                    data={this.state}
+                  />
                 </Collapsibles>
               </section>
               <section class="share-section js-collapsible">
-                <Collapsibles icon="legend_icon fas fa-share-alt" title="Comparte" arrow="fas fas fa-chevron-up legend_arrow">
+                <Collapsibles
+                  icon="legend_icon fas fa-share-alt"
+                  title="Comparte"
+                  arrow="fas fas fa-chevron-up legend_arrow"
+                >
                   <div class="js-collapsible-content">
                     <div class="share_button">
                       <button type="submit" class="share_button_img">
-                        {' '}
+                        {" "}
                         <i class="share_button_img_icon far fa-address-card" />
                         Crear tarjeta
                       </button>
                     </div>
                   </div>
                   <div className="completed_content hidden">
-                    <h3 className="completed_content_title">La tarjeta ha sido creada:</h3>
-                    <a href="#" className="completed_content_url" target="_self" />
-                    <ShareButton icon="completed_content_button_icon fab fa-twitter" name="Compartir en twitter" />
+                    <h3 className="completed_content_title">
+                      La tarjeta ha sido creada:
+                    </h3>
+                    <a
+                      href="#"
+                      className="completed_content_url"
+                      target="_self"
+                    />
+                    <ShareButton
+                      icon="completed_content_button_icon fab fa-twitter"
+                      name="Compartir en twitter"
+                    />
                   </div>
                 </Collapsibles>
               </section>
             </section>
           </main>
         </section>
-        <Footer url="https://www.adalab.es" foto={logoAdalab} title="Awesome profile-cards @ Sense.JS 2019" />
+        <Footer
+          url="https://www.adalab.es"
+          foto={logoAdalab}
+          title="Awesome profile-cards @ Sense.JS 2019"
+        />
       </div>
     );
   }
--- src/components/InputContainer.js
+++ src/components/InputContainer.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import Input from './Inputs';
-import ImageInput from './ImageInput';
-import '../stylesheets/scss/components/ImageInput.scss';
+import React from "react";
+import Input from "./Inputs";
+import ImageInput from "./ImageInput";
+import "../stylesheets/scss/components/ImageInput.scss";
 
 class InputContainer extends React.Component {
   constructor(props) {
     super(props);
-    // this.updateUserInfo = this.updateUserInfo.bind(this);
   }
 
   render() {
@@ -15,19 +14,85 @@ class InputContainer extends React.Component {
 
     return (
       <div>
-        <Input type="text" id="userFullName" label="Nombre completo" classInput="js-name" name="userFullName" placeholder="Hermione Granger" action={actionToPerform} value={info.userFullName} />
+        <Input
+          type="text"
+          id="userFullName"
+          label="Nombre completo"
+          classInput="js-name"
+          name="userFullName"
+          placeholder="Hermione Granger"
+          action={actionToPerform}
+          value={info.userFullName}
+        />
 
-        <Input type="text" id="userJob" label="Puesto" classInput="js-job" name="userJob" placeholder="Front-end developer" action={actionToPerform} value={info.userJob} />
+        <Input
+          type="text"
+          id="userJob"
+          label="Puesto"
+          classInput="js-job"
+          name="userJob"
+          placeholder="Front-end developer"
+          action={actionToPerform}
+          value={info.userJob}
+        />
 
-        <ImageInput name="Imagen de perfil" valueInput="Añadir imagen" classRealButton="fill-in_button js__profile-trigger" displayPicture="img-profile_preview js__profile-preview" />
+        <ImageInput
+          name="Imagen de perfil"
+          valueInput="Añadir imagen"
+          classRealButton="fill-in_button js__profile-trigger"
+          displayPicture="img-profile_preview js__profile-preview"
+        />
 
-        <Input type="tel" id="userPhone" label="Teléfono" classInput="js-phone" name="userPhone" placeholder="+34 678334765" type="tel" minLength="9" pattern="^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$" action={actionToPerform} value={info.userPhone} />
+        <Input
+          type="tel"
+          id="userPhone"
+          label="Teléfono"
+          classInput="js-phone"
+          name="userPhone"
+          placeholder="+34 678334765"
+          type="tel"
+          minLength="9"
+          pattern="^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$"
+          action={actionToPerform}
+          value={info.userPhone}
+        />
 
-        <Input type="text" id="userEmail" label="Email" classInput="js-email" name="userEmail" placeholder="hermione-granger@gmail.com" type="email" pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$" action={actionToPerform} value={info.userEmail} />
+        <Input
+          type="text"
+          id="userEmail"
+          label="Email"
+          classInput="js-email"
+          name="userEmail"
+          placeholder="hermione-granger@gmail.com"
+          type="email"
+          pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
+          action={actionToPerform}
+          value={info.userEmail}
+        />
 
-        <Input type="text" id="userLinkedin" label="Linkedin" classInput="js-linkedin" name="userLinkedin" placeholder="hermionegranger" type="text" action={actionToPerform} value={info.userLinkedin} />
+        <Input
+          type="text"
+          id="userLinkedin"
+          label="Linkedin"
+          classInput="js-linkedin"
+          name="userLinkedin"
+          placeholder="hermionegranger"
+          type="text"
+          action={actionToPerform}
+          value={info.userLinkedin}
+        />
 
-        <Input type="text" id="userGithub" label="Github" classInput="js-github" name="userGithub" placeholder="hermione-granger" type="text" action={actionToPerform} value={info.userGithub} />
+        <Input
+          type="text"
+          id="userGithub"
+          label="Github"
+          classInput="js-github"
+          name="userGithub"
+          placeholder="hermione-granger"
+          type="text"
+          action={actionToPerform}
+          value={info.userGithub}
+        />
       </div>
     );
   }
