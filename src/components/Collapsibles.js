cc91f8cda0656d750762ee3b1ee54e71365feb93
anamontiaga <amontiaga@gmail.com>
2019-09-18 16:37:00 +0200

    add proptypes to collapsibles

--- src/components/Collapsibles.js
+++ src/components/Collapsibles.js
@@ -1,34 +1,42 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "../stylesheets/scss/components/Collapsibles.scss";
 
 class Collapsibles extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      open: false,
-    }
+      open: false
+    };
     this.togglePanel = this.togglePanel.bind(this);
   }
 
   togglePanel(e) {
-    this.setState({ open: !this.state.open })
+    this.setState({ open: !this.state.open });
   }
 
   render() {
     const arrowStyling = this.state.open ? "fa-chevron-up" : "fa-chevron-down";
-    return (<div>
-      <div onClick={(e) => this.togglePanel(e)} className="legend">
-        <i className={this.props.icon} />
-        <h2 className="legend_title">{this.props.title}</h2>
-        <i className={`fas ${arrowStyling} legend_arrow`} />
+    return (
+      <div>
+        <div onClick={e => this.togglePanel(e)} className="legend">
+          <i className={this.props.icon} />
+          <h2 className="legend_title">{this.props.title}</h2>
+          <i className={`fas ${arrowStyling} legend_arrow`} />
+        </div>
+        {this.state.open ? <div>{this.props.children}</div> : null}
       </div>
-      {this.state.open ? (<div>{this.props.children}</div>) : null}
-
-    </div>);
+    );
   }
 }
 
-
+Collapsibles.propTypes = {
+  state: PropTypes.object,
+  togglePanel: PropTypes.func,
+  open: PropTypes.bool,
+  icon: PropTypes.string,
+  title: PropTypes.string
+  // children: PropTypes.element
+};
 
 export default Collapsibles;
