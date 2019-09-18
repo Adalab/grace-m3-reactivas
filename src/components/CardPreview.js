19564ff0e159293dba716ecace6a7412145b2e0a
saramarin23 <saramarin.econ@gmail.com>
2019-09-18 21:03:20 +0200

    save temporary advance

--- src/components/CardPreview.js
+++ src/components/CardPreview.js
@@ -2,6 +2,8 @@ import React from "react";
 import SocialLinkCardPreview from "./SocialLinkCardPreview.js";
 import "../stylesheets/scss/components/CardPreview.scss";
 import Input from "./Inputs";
+import GetAvatar from "./GetAvatar";
+import { url } from "inspector";
 
 class CardPreview extends React.Component {
   constructor(props) {
@@ -14,25 +16,48 @@ class CardPreview extends React.Component {
   // {this.updatePreviewLinkedin()}
 
   render() {
+    const { profile, isAvatarDefault } = this.state;
     return (
       <div className="card js-card ">
         <div className="card_header">
-          <h1 className={this.props.fullnameClass} data-placeholder={this.props.fullname}>
-            {this.props.fullname}{" "}
+          <h1
+            className={this.props.fullnameClass}
+            data-placeholder={this.props.fullname}
+          >
+            {this.props.fullname}
           </h1>
-          <p className={this.props.jobClass} data-placeholder={this.props.jobCard}>
+          <p
+            className={this.props.jobClass}
+            data-placeholder={this.props.jobCard}
+          >
             {this.props.jobCard}
           </p>
         </div>
-        <div className={this.props.imageCard} />
+        <div className={this.props.imageCard} style={{backgroundImage: `url(${image})`} />
         <ul className="card_footer_list">
-          <SocialLinkCardPreview linkClass="js-phone card_link" iconCard="fas fa-mobile-alt" socialLink={this.props.userPhone} />
+          <SocialLinkCardPreview
+            linkClass="js-phone card_link"
+            iconCard="fas fa-mobile-alt"
+            socialLink={this.props.userPhone}
+          />
 
-          <SocialLinkCardPreview linkClass="js-email card_link" socialLink={this.props.userEmail} iconCard="far fa-envelope" />
+          <SocialLinkCardPreview
+            linkClass="js-email card_link"
+            socialLink={this.props.userEmail}
+            iconCard="far fa-envelope"
+          />
 
-          <SocialLinkCardPreview linkClass="js-linkedin card_link" socialLink={this.props.linkedinLink} iconCard="fab fa-linkedin-in" />
+          <SocialLinkCardPreview
+            linkClass="js-linkedin card_link"
+            socialLink={this.props.linkedinLink}
+            iconCard="fab fa-linkedin-in"
+          />
 
-          <SocialLinkCardPreview linkClass="js-github card_link" socialLink={this.props.githubLink} iconCard="fab fa-github-alt" />
+          <SocialLinkCardPreview
+            linkClass="js-github card_link"
+            socialLink={this.props.githubLink}
+            iconCard="fab fa-github-alt"
+          />
         </ul>
       </div>
     );
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -10,14 +10,19 @@ import Collapsibles from "./Collapsibles.js";
 import PalettesContainer from "./PalettesContainer.js";
 import Footer from "./Footer";
 import CardPreview from "./CardPreview";
+import GetAvatar from "./GetAvatar.js";
+import Profile from "./Profile.js";
+import defaultImage from "./defaultImage.js";
 
 class Editor extends React.Component {
   constructor(props) {
     super(props);
     const localStorageData = this.getData();
-    this.state = localStorageData === null ? this.getInitialState() : localStorageData;
+    this.state =
+      localStorageData === null ? this.getInitialState() : localStorageData;
     this.updateCheckboxColor = this.updateCheckboxColor.bind(this);
     this.saveData = this.saveData.bind(this);
+    this.updateAvatar = this.updateAvatar.bind(this);
   }
 
   getInitialState() {
@@ -28,8 +33,23 @@ class Editor extends React.Component {
       userEmail: "",
       userLinkedin: "",
       userGithub: "",
-      palette: "1"
+      palette: "1",
+      isAvatarDefault: true,
+      profile: {
+        avatar: defaultImage
+      }
     };
+  }
+
+  updateAvatar(img) {
+    const { profile } = this.state;
+    this.setState(prevState => {
+      const newProfile = { ...profile, avatar: img };
+      return {
+        profile: newProfile,
+        isAvatarDefault: false
+      };
+    });
   }
 
   updateEventInfo = event => {
@@ -94,52 +114,107 @@ class Editor extends React.Component {
 
   render() {
     const classColor = `card_content palette${this.state.palette}`;
+    const { profile, isAvatarDefault } = this.state;
     return (
       <div>
-        <Header url="/" foto={awesomeProfilePic} destiny="_self" alt="Awesome Profile Cards" />
-        <section class="editor_container">
-          <main class="editor_main">
-            <section class="editor_card">
-              <main class={classColor}>
-                <div class="container">
-                  <ResetButton icon="far fa-trash-alt trash_icon" name="Reset" />
-                  <CardPreview fullnameClass="js-name card_name" fullname={this.updatePreviewName()} jobClass="js-job card_job" jobCard={this.updatePreviewJob()} imageCard="card_img js__profile-image" userPhone={this.state.userPhone} userEmail={this.updatePreviewEmail()} linkedinLink={this.updatePreviewLinkedin()} githubLink={this.updatePreviewGithub()} />
+        <Header
+          url="/"
+          foto={awesomeProfilePic}
+          destiny="_self"
+          alt="Awesome Profile Cards"
+        />
+        <section className="editor_container">
+          <main className="editor_main">
+            <section className="editor_card">
+              <main className={classColor}>
+                <div className="container">
+                  <ResetButton
+                    icon="far fa-trash-alt trash_icon"
+                    name="Reset"
+                  />
+                  <div className="App">
+                    <GetAvatar
+                      avatar={profile.avatar}
+                      isAvatarDefault={isAvatarDefault}
+                      updateAvatar={this.updateAvatar}
+                    />
+
+                    <Profile avatar={profile.avatar} />
+                  </div>
+                  <CardPreview
+                    fullnameclassName="js-name card_name"
+                    fullname={this.updatePreviewName()}
+                    jobclassName="js-job card_job"
+                    jobCard={this.updatePreviewJob()}
+                    imageCard="card_img js__profile-image"
+                    userPhone={this.state.userPhone}
+                    userEmail={this.updatePreviewEmail()}
+                    linkedinLink={this.updatePreviewLinkedin()}
+                    githubLink={this.updatePreviewGithub()}
+                  />
                 </div>
               </main>
             </section>
-            <section class="editor_form js-form">
-              <section class="design_section js-collapsible">
-                <Collapsibles icon="legend_icon far fa-object-ungroup" title="Diseña" arrow="fas fas fa-chevron-up legend_arrow">
-                  <PalettesContainer updateCheckboxColor={this.updateCheckboxColor}></PalettesContainer>
+            <section className="editor_form js-form">
+              <section className="design_section js-collapsible">
+                <Collapsibles
+                  icon="legend_icon far fa-object-ungroup"
+                  title="Diseña"
+                  arrow="fas fas fa-chevron-up legend_arrow"
+                >
+                  <PalettesContainer
+                    updateCheckboxColor={this.updateCheckboxColor}
+                  ></PalettesContainer>
                 </Collapsibles>
               </section>
-              <section class="fill-in_section js-collapsible">
-                <Collapsibles icon="far fa-keyboard legend_icon" title="Rellena" arrow="fas fas fa-chevron-up legend_arrow">
+              <section className="fill-in_section js-collapsible">
+                <Collapsibles
+                  icon="far fa-keyboard legend_icon"
+                  title="Rellena"
+                  arrow="fas fas fa-chevron-up legend_arrow"
+                >
                   <InputContainer updateEventInfo={this.updateEventInfo} />
                 </Collapsibles>
               </section>
-              <section class="share-section js-collapsible">
-                <Collapsibles icon="legend_icon fas fa-share-alt" title="Comparte" arrow="fas fas fa-chevron-up legend_arrow">
-                  <div class="js-collapsible-content">
-                    <div class="share_button">
-                      <button type="submit" class="share_button_img">
+              <section className="share-section js-collapsible">
+                <Collapsibles
+                  icon="legend_icon fas fa-share-alt"
+                  title="Comparte"
+                  arrow="fas fas fa-chevron-up legend_arrow"
+                >
+                  <div className="js-collapsible-content">
+                    <div className="share_button">
+                      <button type="submit" className="share_button_img">
                         {" "}
-                        <i class="share_button_img_icon far fa-address-card" />
+                        <i className="share_button_img_icon far fa-address-card" />
                         Crear tarjeta
                       </button>
                     </div>
                   </div>
                   <div className="completed_content hidden">
-                    <h3 className="completed_content_title">La tarjeta ha sido creada:</h3>
-                    <a href="#" className="completed_content_url" target="_self" />
-                    <ShareButton icon="completed_content_button_icon fab fa-twitter" name="Compartir en twitter" />
+                    <h3 className="completed_content_title">
+                      La tarjeta ha sido creada:
+                    </h3>
+                    <a
+                      href="#"
+                      className="completed_content_url"
+                      target="_self"
+                    />
+                    <ShareButton
+                      icon="completed_content_button_icon fab fa-twitter"
+                      name="Compartir en twitter"
+                    />
                   </div>
                 </Collapsibles>
               </section>
             </section>
           </main>
         </section>
-        <Footer url="https://www.adalab.es" foto={logoAdalab} title="Awesome profile-cards @ Sense.JS 2019" />
+        <Footer
+          url="https://www.adalab.es"
+          foto={logoAdalab}
+          title="Awesome profile-cards @ Sense.JS 2019"
+        />
       </div>
     );
   }
