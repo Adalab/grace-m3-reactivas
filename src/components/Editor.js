ef63583432d5addd57c0e2c3445a16e780e46205
anamontiaga <amontiaga@gmail.com>
2019-09-18 16:16:28 +0200

    install proptypes on Editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logoAdalab from "../images/logo-adalab-80px.png";
+import PropTypes from "prop-types";
 import awesomeProfilePic from "../images/tarjetas-molonas.svg";
 import Header from "./Header";
 import InputContainer from "./InputContainer.js";
@@ -15,8 +16,7 @@ class Editor extends React.Component {
   constructor(props) {
     super(props);
     const localStorageData = this.getData();
-    this.state =
-      localStorageData === null ? this.getInitialState() : localStorageData;
+    this.state = localStorageData === null ? this.getInitialState() : localStorageData;
     this.updateCheckboxColor = this.updateCheckboxColor.bind(this);
     this.saveData = this.saveData.bind(this);
     this.clearForm = this.clearForm.bind(this);
@@ -116,66 +116,30 @@ class Editor extends React.Component {
     const classColor = `card_content palette${this.state.palette}`;
     return (
       <div>
-        <Header
-          url="/"
-          foto={awesomeProfilePic}
-          destiny="_self"
-          alt="Awesome Profile Cards"
-        />
+        <Header url="/" foto={awesomeProfilePic} destiny="_self" alt="Awesome Profile Cards" />
         <section class="editor_container">
           <main class="editor_main">
             <section class="editor_card">
               <main class={classColor}>
                 <div class="container">
-                  <ResetButton
-                    icon="far fa-trash-alt trash_icon"
-                    name="Reset"
-                    action={this.clearForm}
-                  />
-                  <CardPreview
-                    fullnameClass="js-name card_name"
-                    fullname={this.updatePreviewName()}
-                    jobClass="js-job card_job"
-                    jobCard={this.updatePreviewJob()}
-                    imageCard="card_img js__profile-image"
-                    userPhone={this.state.userPhone}
-                    userEmail={this.updatePreviewEmail()}
-                    linkedinLink={this.updatePreviewLinkedin()}
-                    githubLink={this.updatePreviewGithub()}
-                  />
+                  <ResetButton icon="far fa-trash-alt trash_icon" name="Reset" action={this.clearForm} />
+                  <CardPreview fullnameClass="js-name card_name" fullname={this.updatePreviewName()} jobClass="js-job card_job" jobCard={this.updatePreviewJob()} imageCard="card_img js__profile-image" userPhone={this.state.userPhone} userEmail={this.updatePreviewEmail()} linkedinLink={this.updatePreviewLinkedin()} githubLink={this.updatePreviewGithub()} />
                 </div>
               </main>
             </section>
             <section class="editor_form js-form">
               <section class="design_section js-collapsible">
-                <Collapsibles
-                  icon="legend_icon far fa-object-ungroup"
-                  title="Diseña"
-                  arrow="fas fas fa-chevron-up legend_arrow"
-                >
-                  <PalettesContainer
-                    updateCheckboxColor={this.updateCheckboxColor}
-                  ></PalettesContainer>
+                <Collapsibles icon="legend_icon far fa-object-ungroup" title="Diseña" arrow="fas fas fa-chevron-up legend_arrow">
+                  <PalettesContainer updateCheckboxColor={this.updateCheckboxColor}></PalettesContainer>
                 </Collapsibles>
               </section>
               <section class="fill-in_section js-collapsible">
-                <Collapsibles
-                  icon="far fa-keyboard legend_icon"
-                  title="Rellena"
-                  arrow="fas fas fa-chevron-up legend_arrow"
-                >
-                  <InputContainer
-                    updateEventInfo={this.updateEventInfo}
-                    data={this.state}
-                  />
+                <Collapsibles icon="far fa-keyboard legend_icon" title="Rellena" arrow="fas fas fa-chevron-up legend_arrow">
+                  <InputContainer updateEventInfo={this.updateEventInfo} data={this.state} />
                 </Collapsibles>
               </section>
               <section class="share-section js-collapsible">
-                <Collapsibles
-                  icon="legend_icon fas fa-share-alt"
-                  title="Comparte"
-                  arrow="fas fas fa-chevron-up legend_arrow"
-                >
+                <Collapsibles icon="legend_icon fas fa-share-alt" title="Comparte" arrow="fas fas fa-chevron-up legend_arrow">
                   <div class="js-collapsible-content">
                     <div class="share_button">
                       <button type="submit" class="share_button_img">
@@ -186,32 +150,38 @@ class Editor extends React.Component {
                     </div>
                   </div>
                   <div className="completed_content hidden">
-                    <h3 className="completed_content_title">
-                      La tarjeta ha sido creada:
-                    </h3>
-                    <a
-                      href="#"
-                      className="completed_content_url"
-                      target="_self"
-                    />
-                    <ShareButton
-                      icon="completed_content_button_icon fab fa-twitter"
-                      name="Compartir en twitter"
-                    />
+                    <h3 className="completed_content_title">La tarjeta ha sido creada:</h3>
+                    <a href="#" className="completed_content_url" target="_self" />
+                    <ShareButton icon="completed_content_button_icon fab fa-twitter" name="Compartir en twitter" />
                   </div>
                 </Collapsibles>
               </section>
             </section>
           </main>
         </section>
-        <Footer
-          url="https://www.adalab.es"
-          foto={logoAdalab}
-          title="Awesome profile-cards @ Sense.JS 2019"
-        />
+        <Footer url="https://www.adalab.es" foto={logoAdalab} title="Awesome profile-cards @ Sense.JS 2019" />
       </div>
     );
   }
 }
+
+Editor.propTypes = {
+  getData: PropTypes.func,
+  state: PropTypes.object,
+  updateCheckboxColor: PropTypes.func,
+  saveData: PropTypes.func,
+  clearForm: PropTypes.func,
+  resetData: PropTypes.func,
+  forceUpdate: PropTypes.func,
+  setState: PropTypes.object,
+  // LocalStorage ??
+  clearForm: PropTypes.func,
+  updatePreviewName: PropTypes.func,
+  updatePreviewJob: PropTypes.func,
+  updatePreviewEmail: PropTypes.func,
+  updatePreviewLinkedin: PropTypes.func,
+  updatePreviewGithub: PropTypes.func,
+  updateEventInfo: PropTypes.func
+};
 
 export default Editor;
