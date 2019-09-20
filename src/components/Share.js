844ede43b76e1858d2a217e87f51f9fe618a2aa4
saramarin23 <saramarin.econ@gmail.com>
2019-09-20 20:46:38 +0200

    add loader jsx

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,37 +1,39 @@
 import React from "react";
 import "../stylesheets/scss/components/ShareButton.scss";
 import "../stylesheets/scss/core/Variables.scss";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
-import Loader from 'react-loader-spinner'
-
+import Loader from "react-loader-spinner";
 
 function Share() {
   return (
     <section>
+      asdf
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={30000} //3 secs
+      />
+      asdf
       <div className="completed_content hidden">
         <h3 className="completed_content_title">La tarjeta ha sido creada:</h3>
         <a href="#" className="completed_content_url" target="_self"></a>
-        <Loader
-         type="Puff"
-         color="#00BFFF"
-         height={100}
-         width={100}
-         timeout={3000} //3 secs
-
-      />
 
         <button type="submit" className="completed_content_button">
           {" "}
-          <a href="" className="completed_content_button_twitter" target="_blank">
+          <a
+            href=""
+            className="completed_content_button_twitter"
+            target="_blank"
+          >
             <i className="completed_content_button_icon fab fa-twitter"></i>
             Compartir en twitter
           </a>
         </button>
       </div>
-      
     </section>
-   
   );
 }
 
--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -3,15 +3,28 @@ import PropTypes from "prop-types";
 import "../stylesheets/scss/components/ShareButton.scss";
 import "../stylesheets/scss/core/Variables.scss";
 
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+import Loader from "react-loader-spinner";
+
 function ShareButton(props) {
   return (
-    <button type="submit" className="completed_content_button">
-      {" "}
-      <a href="" className="completed_content_button_twitter" target="_blank">
-        <i className={props.icon}></i>
-        {props.name}
-      </a>
-    </button>
+    <div>
+      <Loader
+        type="RevolvingDot"
+        color="#a2deaf"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />
+      <button type="submit" className="completed_content_button">
+        {" "}
+        <a href="" className="completed_content_button_twitter" target="_blank">
+          <i className={props.icon}></i>
+          {props.name}
+        </a>
+      </button>
+    </div>
   );
 }
 
