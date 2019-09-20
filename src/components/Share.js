7f877ff1970f1e8a8cfa2a29a588844c853e81ab
anamontiaga <amontiaga@gmail.com>
2019-09-20 10:45:30 +0200

    finish trying sharing card

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,21 +1,62 @@
 import React from "react";
+import ShareButton from "./ShareButton";
 import "../stylesheets/scss/components/ShareButton.scss";
 import "../stylesheets/scss/core/Variables.scss";
 
-const Share = props => {
-  return (
-    <section>
-      <div className="js-collapsible-content">
-        <div className="share_button">
-          <button type="submit" className="share_button_img">
-            {" "}
-            <i className="share_button_img_icon far fa-address-card" />
-            {props.title}
-          </button>
+class Share extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      class: "hidden",
+      twitterData: ""
+    };
+
+    this.createCard = this.createCard.bind(this);
+    this.handleHiddenClass = this.handleHiddenClass.bind(this);
+    this.sendRequest = this.sendRequest.bind(this);
+  }
+
+  handleHiddenClass() {
+    this.setState({
+      class: ""
+    });
+  }
+
+  createCard(ev) {
+    ev.preventDefault();
+    this.handleHiddenClass();
+    this.sendRequest(this.props.localStorageData);
+  }
+
+  sendRequest(localStorageData) {
+    fetch("https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(localStorageData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          twitterData: data.success === true ? data.cardURL : "ERROR: rellena todos los datos"
+        });
+      });
+  }
+
+  render() {
+    return (
+      <section>
+        <div className="js-collapsible-content">
+          <div className="share_button">
+            <button type="submit" className="share_button_img" onClick={this.createCard}>
+              {" "}
+              <i className="share_button_img_icon far fa-address-card" />
+              {this.props.title}
+            </button>
+          </div>
         </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    );
+  }
+}
 
 export default Share;
--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -3,14 +3,19 @@ import PropTypes from "prop-types";
 import "../stylesheets/scss/components/ShareButton.scss";
 import "../stylesheets/scss/core/Variables.scss";
 
+// TwitterData is undefined
 const ShareButton = props => {
+  const { twitterData } = props;
+  console.log(twitterData);
   return (
     <div className="completed_content hidden">
       <h3 className="completed_content_title">{props.title}</h3>
-      <a href="#" className="completed_content_url" target="_self" />
+      <a href={`${twitterData}`} className="completed_content_url" target="_self" rel="noopener noreferrer">
+        {twitterData}
+      </a>
       <button type="submit" className="completed_content_button">
         {" "}
-        <a href="" className="completed_content_button_twitter" target="_blank">
+        <a href={`https://twitter.com/intent/tweet?text=Aqui tienes mi tarjeta:${twitterData}`} className="completed_content_button_twitter" target="_blank" rel="noopener noreferrer">
           <i className={props.icon}></i>
           {props.name}
         </a>
