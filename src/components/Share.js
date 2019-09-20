422e1ca0fe275b4b0e99a0edccca909f07e01a91
Sara Martín <martingutierrezsara@gmail.com>
2019-09-20 20:25:48 +0200

    first commit

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "../stylesheets/scss/components/ShareButton.scss";
 import "../stylesheets/scss/core/Variables.scss";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+
+import Loader from 'react-loader-spinner'
+
 
 function Share() {
   return (
@@ -8,6 +12,14 @@ function Share() {
       <div className="completed_content hidden">
         <h3 className="completed_content_title">La tarjeta ha sido creada:</h3>
         <a href="#" className="completed_content_url" target="_self"></a>
+        <Loader
+         type="Puff"
+         color="#00BFFF"
+         height={100}
+         width={100}
+         timeout={3000} //3 secs
+
+      />
 
         <button type="submit" className="completed_content_button">
           {" "}
@@ -17,7 +29,9 @@ function Share() {
           </a>
         </button>
       </div>
+      
     </section>
+   
   );
 }
 
